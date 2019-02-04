b3442aba6097ef191e9f81ca47d040b81a7e36cd
xDOASx <benesbit@yahoo.com>
2019-02-04 11:33:46 -0800

    Add attributes to ship class

--- ship.rb
+++ ship.rb
@@ -1,13 +1,17 @@
 class Ship
 
 	def initialize()
-
+		@location = Vector[screen_width / 2, screen_height - 50]
+		#TODO acceleration modifies velocity which modifies position
+		@velocity = Vector[10, 0]
 	end
 
 	def moveLeft
+		@location -= @velocity
 	end
 
 	def mmoveRight
+		@location += @velocity
 	end
 
 	def fireMissle
