09c1c48d2d22326059d3b86c693ce132c15759d7
moehnked <moehnked@oregonstate.edu>
2019-02-20 11:30:26 -0800

    implemented a hash argument for the missile initialization

--- missile.rb
+++ missile.rb
@@ -1,9 +1,9 @@
 class Missile
   attr_accessor :position, :velocity
 
-  def initialize(position, velocity)
-    @position = position
-    @velocity = velocity
+  def initialize(args)
+    @position = args.fetch(:position, Vector[0,0])
+    @velocity = args.fetch(:velocity, Vector[0,10])
   end
 
   def move
